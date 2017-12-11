80d7de22c9e699ec37d3a78d859d16f26a432cf3
Ole Fritsch <olefritsch@icloud.com>
2017-12-11 19:26:54 +0100

    Created bullet prefab

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] float initialSpeed = 200;
+    [SerializeField] float lifetime = 5;
+
+    private Rigidbody rb;
+    private float timeInstatiated;
+    private float lerpT;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(this.transform.up * initialSpeed);
+
+        timeInstatiated = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timeInstatiated > lifetime)
+        {
+            float scale = Mathf.Lerp(this.transform.localScale.x, 0, lerpT);
+            lerpT += Time.deltaTime;
+
+            if (scale < 0.05)
+                Destroy(this.gameObject);
+            else
+                this.transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+}
+
