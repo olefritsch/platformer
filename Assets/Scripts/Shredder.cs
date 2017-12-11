a8c0681191f3ac6fb2e2d78d4971881e8cbdd80e
Ole Fritsch <olefritsch@icloud.com>
2017-12-11 19:34:06 +0100

    Created shredder to destroy 'out-of-bounds' projectiles

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    const string ProjectileIdentifierTag = "Projectile";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ProjectileIdentifierTag)
+            Destroy(other.gameObject);
+    }
+}
