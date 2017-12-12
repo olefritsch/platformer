c592467e8039d73932acf1060813bd25bb4f430c
Ole Fritsch <olefritsch@icloud.com>
2017-12-12 06:22:15 +0100

    Created mock menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Play()
+    {
+        // TODO: Go to some form of player setup scene
+        SceneManager.LoadScene("_Game");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
