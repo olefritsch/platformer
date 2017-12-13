4c2a01ad72184bdbd580d914db36dc463fc02ecf
Ole Fritsch <olefritsch@icloud.com>
2017-12-13 07:55:10 +0100

    Created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance = null;
+
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] Ability[] abilities;
+
+    public enum GameState { Menu, GameSetup, GamePlay, GameEnd }
+    private GameState state;
+
+    private List<GameObject> players;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(this.gameObject);
+    }
+
+    void Start()
+    {
+        players = new List<GameObject>();
+        OnPlayerJoin();
+    }
+
+    public void OnPlayerJoin()
+    {
+        Vector3 spawnPos = new Vector3(-10f + (players.Count * 4), 0f, 0f);
+        GameObject playerObj = Instantiate(playerPrefab, spawnPos, Quaternion.identity);
+        
+        // TODO: Allow players to choose abilities before game starts
+        PlayerController player = playerObj.GetComponent<PlayerController>();
+        player.Ability = abilities[0];
+        player.Ability.Initialize(playerObj);
+    }
+}
