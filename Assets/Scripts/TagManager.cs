d6abc60167e856932495b1defaeccc7e4cda8c86
Ole Fritsch <ole.fritsch@hiq.se>
2018-02-16 12:20:04 +0100

    Changed TagManager from class to struct

--- Assets/Scripts/TagManager.cs
+++ Assets/Scripts/TagManager.cs
@@ -1,11 +1,12 @@
-﻿
-public class TagManager {
-
+﻿public struct TagManager 
+{
+	// Tags
     public const string PlayerIdentifierTag = "Player";
     public const string GroundIdentifierTag = "Ground";
     public const string ProjectileIdentifierTag = "Projectile";
 	public const string DummyIdentifierTag = "Dummy";
-
+	 
+	// Layers
 	public const string PlayerLayer = "Players";
 	public const string ProjectileLayer = "Projectiles";
 }
