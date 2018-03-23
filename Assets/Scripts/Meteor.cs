7518e3b08434d9a18bc7dc42cc9b7b7c42ae6090
Ole Fritsch <ole.fritsch@hiq.se>
2018-03-23 13:50:20 +0100

    Created MeteorShower

---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Meteor : MonoBehaviour 
+{
+	[SerializeField] float movementSpeed = 10f;
+	[SerializeField] float rotationSpeed = 5f;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.rotation = Random.rotation;
+        transform.localScale *= Random.Range(0.85f, 1.15f);
+
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = Vector3.down * movementSpeed;
+		rb.angularVelocity = Random.onUnitSphere * rotationSpeed;
+	}
+
+	void OnCollisionEnter() 
+	{
+		// TODO: Explode meteor and spawn rubble
+	}
+
+
+}
---
+++ Assets/Scripts/SpawnPrefabs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPrefabs : MonoBehaviour 
+{
+	[SerializeField] GameObject prefab;
+
+	[SerializeField] float startDelay;
+	[SerializeField] float spawnDelay;
+	[SerializeField] float spawnOffset;
+
+	[Range(1, 50)]
+	[SerializeField] int maxNumberOfPrefabs;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(MakeItRain ());
+	}
+	
+	// Update is called once per frame
+	IEnumerator MakeItRain() 
+	{
+		yield return new WaitForSeconds(startDelay);
+
+		int numberOfPrefabs = 0;
+
+		while (numberOfPrefabs < maxNumberOfPrefabs) 
+		{
+			if (Random.value > 0.5f) 
+			{
+				Vector3 spawnPos = transform.position + new Vector3(Random.Range(-spawnOffset, spawnOffset), 0f, 0f);
+				Instantiate(prefab, spawnPos, Quaternion.identity, transform);
+				numberOfPrefabs++;
+			}
+
+			yield return new WaitForSeconds(spawnDelay);
+		}
+	}
+}
